8b86402433f6b77b8e1251f1ae2cda6b9f401ebf
sho kisaragi <sho@oshiire.to>
2017-08-05 22:26:58 +0900

    メソッドの細分化

--- lib/reins_agent.rb
+++ lib/reins_agent.rb
@@ -8,41 +8,62 @@ require "ipaddr"
 require "socket"
 
 module ReinsAgent
-  def exec(command)
-    s = TCPSocket.open(ReinsAgent.server_host, ReinsAgent.server_port)
-    s.puts command
-    response = s.read
-    s.close if s
-    response
+  class << self
+    def run_agent
+      ReinsAgent.logger.info("Reins Agent #{VERSION} を #{ReinsAgent.client_port} で起動します")
+      throw unless (@cert_key = ReinsAgent.exec("#{ReinsAgent.client_key} auth").chomp)
+      ReinsAgent.logger.debug("認証キー : #{@cert_key}")
+      @agent = TCPServer.new(ReinsAgent.client_port)
+    rescue => e
+      ReinsAgent.logger.fatal("Reins Agent が起動できませんでした: #{e}")
+      exit
+    end
+
+    def define_value(r)
+      @addr = IPAddr.new(r.peeraddr[3]).native.to_s
+      @keycode, @command, @options = r.gets.chomp.split
+      ReinsAgent.logger.debug("addr = #{@addr}, keycode = #{@keycode}, command = #{@command}, options = #{@options}")
+    end
+
+    def exit_agent
+      ReinsAgent.logger.info("Reins Agent #{VERSION} を終了します")
+      ReinsAgent.exec("#{@cert_key} delete")
+      @agent.close
+      exit
+    end
+    # サーバへコマンドを送信し、その返り値を取得する
+    # == パラメータ
+    # command:: サーバへ送信するコマンド+オプションを指定
+    # == 返り値
+    # response:: サーバからの返り値(改行付き/複数行)
+    def exec(command)
+      s = TCPSocket.open(ReinsAgent.server_host, ReinsAgent.server_port)
+      s.puts command
+      response = s.read
+      s.close if s
+      response
+    end
   end
 
   def start
-    exit unless (cert_key = ReinsAgent.exec("#{ReinsAgent.client_key} auth").chomp)
+    run_agent
 
-    agent = TCPServer.new(ReinsAgent.client_port)
-    ReinsAgent.logger.info("Reins Agent #{VERSION} を #{ReinsAgent.client_port} で起動します")
-
-    puts ReinsAgent.exec("#{cert_key} list")
+    puts ReinsAgent.exec("#{@cert_key} list")
 
     loop do
       begin
-        Thread.start(agent.accept) do |r|
-          addr = IPAddr.new(r.peeraddr[3]).native.to_s
-          keycode, command, options = r.gets.chomp.split
-          ReinsAgent.logger.debug("addr = #{addr}, keycode = #{keycode}, command = #{command}, options = #{options}")
-
-          if cert_key == keycode
-            s.puts("OK")
+        Thread.start(@agent.accept) do |r|
+          define_value(r)
+          if @cert_key == @keycode
+            r.puts("OK")
           end
+          r.close
         end
-      rescue Interrupt => e
-        ReinsAgent.logger.info(e.to_s)
-        ReinsAgent.exec("#{cert_key} delete")
-        agent.close
-        exit
+      rescue Interrupt
+        exit_agent
       end
     end
   end
 
-  module_function :start, :exec
+  module_function :start
 end
